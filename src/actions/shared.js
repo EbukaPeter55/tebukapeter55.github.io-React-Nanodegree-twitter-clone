dcceb5ddd487ce5cf28f58d38e23344491ab04ed
Ebuka Peter <peterdbrainy5@gmail.com>
2021-11-01 04:39:36 +0100

    Add the loading bar component to display before the component loads data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,17 +2,20 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveTweets } from './tweets';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis'
 
 // action creator Using redux thunk middleware
 export function handleInitialData () {
     return (dispatch) => {
+        dispatch(showLoading()) //Dispatches the loading bar once we load the component
         return getInitialData()
         .then(({users, tweets}) => {
             dispatch(receiveUsers(users));
             dispatch(receiveTweets(tweets));
             dispatch(setAuthedUser(AUTHED_ID));
+            dispatch(hideLoading());
         })
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard';
-
+import LoadingBar from 'react-redux-loading';
 
 class App extends Component {
  
@@ -14,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <LoadingBar/>
      {/* If authedUser is null, display null, else render 
     the dashboard*/}
      {
